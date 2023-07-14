079eb8c6ff7a4869792b9c732e5598242706a2c4
Nera <omega3050h@mail.ru>
2023-07-14 17:25:56 +0300

    router?

--- server/index.js
+++ server/index.js
@@ -5,8 +5,14 @@ const models = require('./models/models')
 const cors = require('cors')
 
 const app = express();
+app.use(cors())
+app.use(express.json())
 
 const PORT = process.env.PORT;
+
+app.get('/', (req,res) => {
+    res.status(200).json({message: 'Okay'})
+})
 
 const start = async () => {
     try {
---
+++ server/routes/index.js
@@ -0,0 +1,6 @@
+const Router = require('express')
+const router = new Router()
+
+router.use('/',)
+
+module.exports = router
