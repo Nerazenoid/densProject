1a028e8b75729a8c456b0cc449ffd25a466d72c2
Nerazenoid <omega3050h@mail>
2023-07-28 00:42:41 +0300

    test

--- client/src/http/appointmentAPI.js
+++ client/src/http/appointmentAPI.js
@@ -1,6 +1,7 @@
 import {$host} from "./index";
 
 export const getByDay = async (doctor_id) => {
+    if(!doctor_id) {doctor_id = 0}
     const {data} = await $host.get('/api/appointment/getday?doctor_id=' + doctor_id)
     return data
 }
