799323c9eb96a0ca285b7d728ffdf5769a15ff71
Nerazenoid <omega3050h@mail>
2023-10-14 15:26:26 +0300

    SHIT SHIT SHIT

--- client/src/pages/MakeNewAppt.jsx
+++ client/src/pages/MakeNewAppt.jsx
@@ -2,7 +2,7 @@ import { getByDay, getDays } from "../http/appointmentAPI.js";
 import TimePicker from '../components/calendar/timePicker.jsx';
 import { useContext, useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import DaysPicker from './components/calendar/daysPicker';
+import DaysPicker from '../components/calendar/daysPicker';
 import { Context } from '../index';
 import Modal from '../components/modal.jsx';
 import { useParams } from 'react-router-dom';
